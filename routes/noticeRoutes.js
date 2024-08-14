38640b5baad0e8913b9936013be1a2cccd73518d
Ali nadhum <alinadhum1998@gmail.com>
2024-08-14 22:45:28 +0300

    update notice route

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -3,7 +3,7 @@ const { verifyTokenAdmin } = require("../middlewares/verifyToken");
 const {signUserNotice, verifieCode, getAllUsers, deleteUser} = require("../controllers/noticeController")
 
 
-router.get("/" , verifyTokenAdmin, getAllUsers);
+router.get("/" , getAllUsers);
 router.delete("/:id" , verifyTokenAdmin,deleteUser)
 router.post("/signup" , signUserNotice);
 router.post("/verifiecode" , verifieCode);
